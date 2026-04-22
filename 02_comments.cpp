3faac17e8bf28ae2ec5b943a7606d7c96dd8e588
Thoufeq <shaikthoufeq0@gmail.com>
2026-04-22 16:38:54 +0530

    Step 2: Documenting code using single and multi-line comments

---
+++ 02_comments.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // This is a single line comment
+    /* This is 
+        Multi-Line comment.
+        Note : Nesting of comments is not allowed.
+    */
+    cout << "Hello World!" << endl;
+}
