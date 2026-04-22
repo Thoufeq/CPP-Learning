5bc72592826449bd3758d66c792fbdcae9540a93
Thoufeq <shaikthoufeq0@gmail.com>
2026-04-22 17:26:49 +0530

    Explanation of Statements in C++

---
+++ 06_Statements.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    
+    /* 
+    
+    1. A statement is a basic unit of computation in a C++ program
+    2. Every C++ program is a collection of statements organized in a certain way to achieve some goal
+    3. Statements end with a semicolon in C++ (;)'
+    4. Statements are executed in order from top to bottom when the program is run
+    5. Execution keeps going until there is a statement causing the program to terminate, or run another sequence of statements
+    
+    */
+    cout << "Hello World!" << endl; // This line is a statement.
+    return 0;
+}
