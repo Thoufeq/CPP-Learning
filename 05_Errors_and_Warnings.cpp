42448a505fb36b53115c687f5d9a1c274d28c727
Thoufeq <shaikthoufeq0@gmail.com>
2026-04-22 17:12:18 +0530

    Added Errors and Warnings

---
+++ 05_Errors_and_Warnings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Compiletime Error(The code doesnt get compiled(no binary conversion))
+    // Causes of compile-time error - syntax, declaration, type and header error.
+    cout << "Hello" << endl // Semicolon is missing
+    
+    
+    //Runtime Error
+    cout << 7/0; // warning: division by zero [-Wdiv-by-zero]
+
+    return 0;
+}
