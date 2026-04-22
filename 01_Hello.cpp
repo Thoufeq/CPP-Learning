f567d9359b7c23ba9af74d55e536338d95a4bf48
Thoufeq <shaikthoufeq0@gmail.com>
2026-04-22 16:38:47 +0530

    Step 1: Basic syntax and printing to console

---
+++ 01_Hello.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0; // The return 0; at the end of a C++ main() function 
+              // is a signal from your program to the Operating System (OS). 
+              // Since main() is defined with an int return type, 
+              // it must return an integer once it finishes its job.
+}
